1865a43cafe4c574838702719285efe837f6a613
Obd2004 <obidjonabd1097@gmail.com>
2023-01-16 12:49:01 +0500

    new commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+
 function Home() {
+
+  const dispatch = useDispatch()
+  const productAdd = (e)=>{
+    e.preventDefault()
+    let val = e.target.product.value
+    dispatch({type:'ADD' , payload:{'meva':val}})
+    console.log(val);
+  }
+
   return (
     <div>
         <h1>Home</h1>
+        <form action="/" onSubmit={productAdd}>
+          <input name='product' type="text" placeholder='meva' />
+          <button type='submit'>Add</button>
+        </form>
     </div>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import {createStore, combineReducer} from 'redux'
+import {createStore, combineReducers} from 'redux'
 import shop from './Shop.js'
 
-const reducer = combineReducer({
-    shop,   
+const reducer = combineReducers({
+    shop,
 })
+
 const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
